7aa251ad937b1a002f662a01d73768eb6686f27f
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 02:36:55 +0200

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """BasicCache inherits from BaseCaching and implements a basic caching system"""
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value for the key key."""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        return self.cache_data.get(key, None)
