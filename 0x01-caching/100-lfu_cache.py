86ea35acbf7ebb37570a010d9e494a3dc9cb9af4
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 03:04:56 +0200

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,43 +1,51 @@
 #!/usr/bin/env python3
+"""
+LFUCache module
+This module provides a caching system with LFU replacement policy.
+"""
 
 from base_caching import BaseCaching
 from collections import defaultdict
 
 class LFUCache(BaseCaching):
-    """LFUCache inherits from BaseCaching and implements LFU caching system"""
+    """
+    LFUCache class
+    Inherits from BaseCaching and implements a LFU caching system.
+    """
 
     def __init__(self):
-        """Initialize the class"""
+        """ Initialize the cache
+        """
         super().__init__()
-        self.frequency = defaultdict(int)
+        self.freq = defaultdict(int)
         self.order = []
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data the item value for the key key."""
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.frequency[key] += 1
-                self.order.remove(key)
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                min_freq = min(self.frequency.values())
-                lfu_keys = [k for k, v in self.frequency.items() if v == min_freq]
-                if len(lfu_keys) == 1:
-                    discarded_key = lfu_keys[0]
-                else:
-                    discarded_key = next(k for k in self.order if k in lfu_keys)
-                del self.cache_data[discarded_key]
-                del self.frequency[discarded_key]
-                self.order.remove(discarded_key)
-                print(f"DISCARD: {discarded_key}")
+                self.freq[key] += 1
+            else:
+                self.freq[key] = 1
+                self.order.append(key)
             self.cache_data[key] = item
-            self.frequency[key] += 1
-            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                min_freq = min(self.freq.values())
+                candidates = [k for k in self.order if self.freq[k] == min_freq]
+                if len(candidates) > 1:
+                    discard = candidates[0]
+                else:
+                    discard = candidates[0]
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.freq[discard]
+                self.order.remove(discard)
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
+        """ Get an item by key
+        """
         if key in self.cache_data:
-            self.frequency[key] += 1
-            self.order.remove(key)
-            self.order.append(key)
+            self.freq[key] += 1
             return self.cache_data[key]
         return None
