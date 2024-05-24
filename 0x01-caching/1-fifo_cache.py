768c415f8d2a77fcf542e0a7583d4fdbd7ea85a0
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 02:56:47 +0200

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """FIFOCache inherits from BaseCaching and implements FIFO caching system"""
+
+    def __init__(self):
+        """Initialize the class"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value for the key key."""
+        if key is not None and item is not None:
+            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.order.pop(0)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+            if key not in self.order:
+                self.order.append(key)
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        return self.cache_data.get(key, None)
