12d4b3f732681308b4fbd230270256b2b125ffd6
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 03:07:28 +0200

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+"""
+BasicCache module
+This module provides a basic caching system with no limits.
+"""
 
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-    """BasicCache inherits from BaseCaching and implements a basic caching system"""
+    """
+    BasicCache class
+    Inherits from BaseCaching and implements a basic caching system.
+    """
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data the item value for the key key."""
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
+        """ Get an item by key
+        """
         return self.cache_data.get(key, None)
