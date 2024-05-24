c78ffb095acddfb275da743ba14de6983c88e36a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 03:07:01 +0200

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
+"""
+FIFOCache module
+This module provides a caching system with FIFO replacement policy.
+"""
 
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
-    """FIFOCache inherits from BaseCaching and implements FIFO caching system"""
+    """
+    FIFOCache class
+    Inherits from BaseCaching and implements a FIFO caching system.
+    """
 
     def __init__(self):
-        """Initialize the class"""
+        """ Initialize the cache
+        """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data the item value for the key key."""
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded_key = self.order.pop(0)
-                del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
-            self.cache_data[key] = item
-            if key not in self.order:
+            if key not in self.cache_data:
                 self.order.append(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = self.order.pop(0)
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
+        """ Get an item by key
+        """
         return self.cache_data.get(key, None)
