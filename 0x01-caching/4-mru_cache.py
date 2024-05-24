6508a4133830a5d8ac7fd320d0ba2434ff86eead
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 03:05:33 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
+"""
+MRUCache module
+This module provides a caching system with MRU replacement policy.
+"""
 
 from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
-    """MRUCache inherits from BaseCaching and implements MRU caching system"""
+    """
+    MRUCache class
+    Inherits from BaseCaching and implements a MRU caching system.
+    """
 
     def __init__(self):
-        """Initialize the class"""
+        """ Initialize the cache
+        """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data the item value for the key key."""
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded_key = self.order.pop(-1)
-                del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
-            self.cache_data[key] = item
-            if key in self.order:
+            if key in self.cache_data:
                 self.order.remove(key)
             self.order.append(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = self.order.pop(-1)
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
+        """ Get an item by key
+        """
         if key in self.cache_data:
             self.order.remove(key)
             self.order.append(key)
