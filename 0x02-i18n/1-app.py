5227abc441a841d59c0ea3a3094238be71f62bd7
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-29 05:08:42 +0200

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app with Babel setup.
+"""
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 class Config:
+    """
+    Configuration class for Babel.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -13,6 +20,11 @@ babel = Babel(app)
 
 @app.route('/')
 def index():
+    """
+    Route for the index page.
+    Returns:
+        Rendered HTML template for the index page.
+    """
     return render_template('1-index.html')
 
 if __name__ == "__main__":
